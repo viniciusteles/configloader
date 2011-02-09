81283353c2aeed2649d183e43a822ac6cbb34414
Vinicius Teles <vinicius@improveit.com.br>
2011-02-09 17:12:45 -0300

    Improved the message that is displayed when the config file can't be located.
    Since the location depends on the value of environment variables, suchs as RAILS_ENV and RAILS_ROOT, it's useful to know no only the full path of the config file, but also the value of these variables, which are used in the composition of the full path.

--- lib/config_loader/missing_config_file_error.rb
+++ lib/config_loader/missing_config_file_error.rb
@@ -1,7 +1,12 @@
 module ConfigLoader
   class MissingConfigFileError < RuntimeError
     def initialize(filename)
-      super("The config file couldn't be found: #{filename}")
+      message = [ "The config file couldn't be found: #{filename}",
+                  "ENV['RAILS_ENV'] = '#{ENV["RAILS_ENV"]}'",
+                  "ENV['RACK_ENV'] = '#{ENV["RACK_ENV"]}'",
+                  "ENV['RAILS_ROOT'] = '#{ENV["RAILS_ROOT"]}'" ]
+            
+      super(message.join("\n"))
     end
   end
 end
